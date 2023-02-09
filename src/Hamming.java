9cf67c0261ba19b926c2ff19b074877fa717555c
PunchedYou <nibot04@gmail.com>
2023-02-10 00:53:24 +0100

    ean

--- src/Hamming.java
+++ src/Hamming.java
@@ -1,27 +1,43 @@
+import dstrukturen.DynArray;
+
 public class Hamming {
     private char xOR(char a, char b) {
         return a == b ? '0' : '1';
     }
 
+    private char isEven(String s) {
+        return (s.length() - s.replace("1", "").length() % 2) == 0 ? '0' : '1';
+    }
+
     public String hammingCode(String s) {
-        char p0 = xOR(xOR(s.charAt(2), s.charAt(4)), s.charAt(6));
-        char p1 = xOR(xOR(s.charAt(2), s.charAt(5)), s.charAt(6));
-        char p2 = xOR(xOR(s.charAt(4), s.charAt(5)), s.charAt(6));
+        return "";
+    }
+
+    public String hammingPruefen(String s) {
+        /*
+         * char p0 = xOR(xOR(s.charAt(2), s.charAt(4)), s.charAt(6));
+         * char p1 = xOR(xOR(s.charAt(2), s.charAt(5)), s.charAt(6));
+         * char p2 = xOR(xOR(s.charAt(4), s.charAt(5)), s.charAt(6));
+         */
+
+        char p0 = isEven("" + s.charAt(2) + s.charAt(4) + s.charAt(6));
+        char p1 = isEven("" + s.charAt(2) + s.charAt(5) + s.charAt(6));
+        char p2 = isEven("" + s.charAt(4) + s.charAt(5) + s.charAt(6));
 
         p0 = xOR(p0, s.charAt(0));
         p1 = xOR(p1, s.charAt(1));
         p2 = xOR(p2, s.charAt(3));
 
-        String p = "" + p2 + p1 + p0;
-        int x = Integer.parseInt(p, 2);
-        System.out.println("Fehler an der Stelle: " + x);
+        String pp = "" + p2 + p1 + p0;
+        int pos = Integer.parseInt(pp, 2);
+        System.out.println("Fehler an der Stelle: " + pos);
 
         String r = "";
         for (int i = 0; i < s.length(); i++) {
-            if (x - 1 != i) {
+            if (pos - 1 != i) {
                 r += s.charAt(i);
             } else {
-                r += Integer.parseInt(String.valueOf(s.charAt(i))) + 1 % 2;
+                r += Character.getNumericValue(s.charAt(i)) + 1 % 2;
             }
         }
 
@@ -29,23 +45,41 @@ public class Hamming {
     }
 
     public String eanErstellen(String s) {
-        return s + eanAnhang(s);
-    }
-
-    public char eanAnhang(String s) {
         int a = 0;
         int b = 0;
 
         for (int i = 0, j = 1; j < 12; i += 2, j += 2) {
-            a += Integer.parseInt(String.valueOf(s.charAt(i)));
-            b += Integer.parseInt(String.valueOf(s.charAt(j)));
+            a += Character.getNumericValue(s.charAt(i));
+            b += Character.getNumericValue(s.charAt(j));
         }
 
-        int x = 10 - ((a + (3 * b)) % 10);
-        return (char) (x + '0');
+        return s + (10 - ((a + (3 * b)) % 10));
     }
 
     public boolean eanPruefen(String s) {
-        return eanAnhang(s.substring(0, 12)) == s.charAt(12);
+        return eanErstellen(s.substring(0, 12)).equals(s);
+    }
+
+    public String eanErstellen2(String s) {
+        DynArray<Integer> darr = new DynArray<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            darr.append(Character.getNumericValue(s.charAt(i)));
+        }
+
+        // darr@0 = ungreade, darr@1 = gerade
+
+        while (darr.getLength() > 2) {
+            darr.setItem(0, darr.getItem(0) + darr.getItem(2));
+            darr.delete(2);
+            darr.setItem(1, darr.getItem(1) + darr.getItem(2));
+            darr.delete(2);
+        }
+
+        return s + (10 - ((darr.getItem(0) + (3 * darr.getItem(1))) % 10));
+    }
+
+    public boolean eanPruefen2(String s) {
+        return eanErstellen2(s.substring(0, 12)).equals(s);
     }
 }
